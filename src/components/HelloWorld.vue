f5d650fa58892b67ebc6c7f76111308e72961ef5
quanghm27 <quanghm27@gmail.com>
2023-02-14 10:26:18 +0700

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increaseCount() {
+      this.count = this.count + 1
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button class="bg-orange-400 rounded-sm p-5" @click="increaseCount">Click me</button>
+    <p>count is {{ count }}</p>
+  </div>
+</template>
